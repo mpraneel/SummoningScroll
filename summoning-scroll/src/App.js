73d36838f41496e25a68b6ffba374e3c2b705be2
Cameron Edwards <cvedward@ncsu.edu>
2025-02-08 20:00:10 -0500

    Added the transition from the intro page to login

--- summoning-scroll/src/App.js
+++ summoning-scroll/src/App.js
@@ -3,12 +3,14 @@ import Login from "./pages/Login";
 import {  BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar'; 
 import Dashboard from "./pages/Dashboard";
+import Intro from "./pages/Intro";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Intro />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
